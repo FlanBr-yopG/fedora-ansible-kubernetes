fa7f4886fc7e16a8d914c3e8d15a77db29a4ebb4
Brian Flannery <brian.flannery@pearson.com>
2017-10-24 13:53:25 -0600

    Make sure to run Ansible; offer a "quick" vagrant provision option.

--- vagrant.sh
+++ vagrant.sh
@@ -2,16 +2,21 @@
 
 main() {
   set -ex
-  vagrant reload
-  vagrant up
+  [[ quick == $1 ]] || {
+    vagrant reload
+    vagrant up
+  }
   [[ -f ./kube-master-priv ]] || cp ./.vagrant/machines/kube-master/virtualbox/private_key ./kube-master-priv
   vagrant provision
+  sleep 120;
+  vagrant ssh -c "sudo bash /vagrant/vm_scripts/go_ansible.sh"
 }
 final() {
   nohup bash -c "sleep 120; rm -f ./kube-master-priv" &> ./nohup1.log &
   echo "NOTE: Scheduled final() cleanup." 1>&2
 }
 
-main
+trap final EXIT
+main "$@"
 
 #
