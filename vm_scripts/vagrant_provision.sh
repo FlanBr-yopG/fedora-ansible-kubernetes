88af3bbbe41181c2f92b2e77e08b5ed52dafc116
Brian Flannery <brian.flannery@pearson.com>
2017-10-27 11:15:02 -0600

    better logs and grep regexps.

--- vm_scripts/vagrant_provision.sh
+++ vm_scripts/vagrant_provision.sh
@@ -2,11 +2,13 @@
 
 dnf install -y ansible git python-netaddr
 egrep '^# hosts v 0 0 1' /etc/hosts || {
-  echo '10.251.240.100 kube-master.vagrant kube-master' >> /etc/hosts
-  echo '10.251.240.101 kube-node01.vagrant kube-node01' >> /etc/hosts
-  echo '10.251.240.102 kube-node02.vagrant kube-node02' >> /etc/hosts
-  echo '10.251.240.103 kube-node03.vagrant kube-node03' >> /etc/hosts
-  echo '# hosts v 0 0 1' >> /etc/hosts
+  {
+    echo '10.251.240.100 kube-master.vagrant kube-master'
+    echo '10.251.240.101 kube-node01.vagrant kube-node01'
+    echo '10.251.240.102 kube-node02.vagrant kube-node02'
+    echo '10.251.240.103 kube-node03.vagrant kube-node03'
+    echo '# hosts v 0 0 1'
+  } >> /etc/hosts
 }
 set -ex
 cd / ;
@@ -20,7 +22,7 @@ cd ansi-kube ;
   echo "kube-node02.vagrant" >> inventory
   echo "kube-node03.vagrant" >> inventory
   echo -e "\\nansible_ssh_user: vagrant" >> group_vars/all.yml
-}
+} > /tmp/kubernetes_contrib_ansible_inventory.log 2>&1
 [[ -f /root/.ssh/id_rsa ]] || {
   umask 0077
   [[ -e /vagrant/kube-master-priv ]] \
@@ -29,22 +31,23 @@ cd ansi-kube ;
   ssh-keygen -y -f /root/.ssh/id_rsa > /root/.ssh/id_rsa.pub
   cat /root/.ssh/id_rsa.pub >> /root/.ssh/authorized_keys
   cat /root/.ssh/id_rsa.pub >> /home/vagrant/.ssh/authorized_keys
-}
+} > /tmp/id_rsa.fix.log 2>&1
+export PATH="$PATH:/usr/local/bin"
 which kubectl || {
   curl -sLO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
   chmod +x ./kubectl
   mv ./kubectl /usr/local/bin/kubectl
-}
-yum list installed docker || yum -y install docker;
-systemctl status rhel-push-plugin.socket | egrep '^ *Active: active (running)' || {
+} > /tmp/kubectl.fix.log 2>&1
+yum list installed docker || yum -y install docker > /tmp/yum_install_docker.log 2>&1
+systemctl status rhel-push-plugin.socket | egrep '^ *Active: active \((listening|running)\)' || {
   yum -y remove container-selinux ;
   yum -y install container-selinux ;
   systemctl start rhel-push-plugin.socket;
   sleep 5;
-  systemctl status rhel-push-plugin.socket | egrep '^ *Active: active (running)'
+  systemctl status rhel-push-plugin.socket | egrep '^ *Active: active \((listening|running)\)'
   systemctl enable docker
   systemctl start docker
-  systemctl status docker | egrep '^ *Active: active (running)'
+  systemctl status docker | egrep '^ *Active: active \(running\)'
 } > /tmp/rhel-push-plugin.socket.fix.log 2>&1
 
 #
